6591bae1c35e521b81e94b3d1b5b5ac99d8f30ee
Brian <brianrlam@gmail.com>
2024-06-26 17:47:10 -0400

    incorporate basic useState

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,19 @@
+'use client';
+import { useState } from "react";
+
 export default function Page() {
-  return <p>Home Page</p>;
+  const [waterConsumed, setWaterConsumed] = useState(0);
+  const [waterGoal, setWaterGoal] = useState(32);
+
+
+  return (
+    <div>
+      <h1>Water Tracker</h1>
+      <p>Water Consumed: {waterConsumed} oz</p>
+      <p>Water Goal: {waterGoal} oz</p>
+      <button onClick={() => setWaterConsumed(waterConsumed + 1)}>Add 1 oz</button>
+      <button onClick={() => setWaterConsumed(waterConsumed - 1)}>Remove 1 oz</button>
+      <button onClick={() => setWaterConsumed(0)}>Reset</button>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Traxquart</h1>
+    <div style={{ width: "100vw", height: "100vh", position: "relative"}}>
+      <Image
+        alt='water'
+        src='/water.jpg'  
+        layout='fill'
+        objectFit='cover'
+      />
     </div>
   );
 }
